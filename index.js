1493f36b2bb13e9ba277ab2e5ee1f6f005324394
mutanim24 <mutanim24@gmail.com>
2023-05-20 22:11:11 +0600

    .env file updated

--- index.js
+++ index.js
@@ -9,11 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// user: doll_mart
-// pass: WnDs3y2khxNS0Qyy
-
-
-
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.z12trsh.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
